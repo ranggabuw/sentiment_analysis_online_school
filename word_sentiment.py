90ee0dfee216b8d7c2f2d9e9d5a2f2d45273f421
Rangga <rangga@Ranggas-MacBook-Air.local>
2023-10-12 20:36:27 +0700

    add title

--- word_sentiment.py
+++ word_sentiment.py
@@ -1,19 +1,21 @@
 import streamlit as st
 
-from pyngrok import ngrok
 from joblib import load
 
 trained_model = load('model_sentiment.pkl')
 
+st.title("Sentimen Komentar Terhadap Sekolah Daring")
+st.write("# ")
+
 form = st.form(key='my-form')
 
-kalimat = form.text_input('Enter your sentence:')
+kalimat = form.text_input('Masukkan Kalimat:')
 
-submit = form.form_submit_button('Get the sentiment!')
+submit = form.form_submit_button('Cari sentimennya!')
 
 if submit:
     y_pred = trained_model.predict([kalimat])
-    st.write('The sentence has a sentiment: ', y_pred[0])
+    st.write('Sentimen dari kalimat tersebut: ', y_pred[0])
 
 # ngrok_tunnel = ngrok.connect(8501)
 # print('Public URL:', ngrok_tunnel.public_url)
